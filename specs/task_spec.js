61600c0069e2ea877935d75ae1fa89aae203ff3a
staceynapier <staceynapier86@gmail.com>
2017-09-01 16:06:16 +0100

    first test set up for task

---
+++ specs/task_spec.js
@@ -0,0 +1,21 @@
+var assert = require('assert');
+var Task = require('../task.js');
+
+describe("Task", function() {
+
+  var eating;
+
+  beforeEach(function() {
+    eating = new Task(1, 3, 5);
+  })
+
+  it("should have a difficulty score", function() {
+    assert.strictEqual(eating.difficulty, 1);
+  })
+
+})
+//
+//   A task has a difficulty level out of 5
+// A task has an urgency level out of 5
+// A task has a reward- money back
+// A task should be able to be marked as completed - true or false
