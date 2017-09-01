2cae95acd2491c0c7754812aa2d0e325bacea05f
staceynapier <staceynapier86@gmail.com>
2017-09-01 15:48:50 +0100

    first test - should have a name, passing

---
+++ hero.js
@@ -0,0 +1,7 @@
+var Hero = function( name, favFood ) {
+  this.name = name;
+  this.favFood = favFood;
+  this.health = 0;
+  this.tasks = [];
+}
+module.exports = Hero;
--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -1,14 +1,16 @@
 var assert = require('assert');
-var Record = require('../hero.js')
+var Hero = require('../hero.js');
 
 describe("Hero", function() {
 
   var hero;
 
-  beforeEach(function(){
-    herp = new Hero("Superman", "cheeseburger")
-  })
+  beforeEach(function() {
+    hero = new Hero("Superman", "cheeseburger")
+  });
 
-  it(should have a name, function()) {
-    assert.strictEqual(hero.name(), "Superman");
-  }
+  it("should have a name", function() {
+    assert.strictEqual(hero.name, "Superman");
+  });
+
+})
